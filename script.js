70b103b2e6cbc77b31ea1b320bbacdb6cb1f5807
Ofra <ofraontheroad@gmail.com>
2021-07-29 02:35:22 +0300

    Created forecast live data using the API

--- script.js
+++ script.js
@@ -29,27 +29,45 @@ let h1 = document.querySelector("#date-time");
 h1.innerHTML = `${day} ${hour}:${minutes}`;
 
 // display forecast:
+
+function formatDay(timestemp) {
+  let date = new Date(timestemp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Tue", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  // console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` 
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6)
+      forecastHTML =
+        forecastHTML +
+        ` 
                 <div class="col-2">
-                  <div class="weather-forecast-date">${day}</div>
+                  <div class="weather-forecast-date">${formatDay(
+                    forecastDay.dt
+                  )}</div>
                   <img
-                    src="http://openweathermap.org/img/wn/04n@2x.png"
+                    src="http://openweathermap.org/img/wn/${
+                      forecastDay.weather[0].icon
+                    }@2x.png"
                     alt="broken clouds"
                     width="50"
                   />
                   <div class="weather-forecast-temperature">
-                    <span class="weather-forecast-temperature-max"> 18º </span>
-                    <span class="weather-forecast-temperature-min"> 12º </span>
+                    <span class="weather-forecast-temperature-max"> ${Math.round(
+                      forecastDay.temp.max
+                    )}º </span>
+                    <span class="weather-forecast-temperature-min"> ${Math.round(
+                      forecastDay.temp.min
+                    )}º </span>
                   </div>
                 </div>
               `;
